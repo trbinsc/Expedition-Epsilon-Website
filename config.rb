92503cda5c0f3b0e7dd5ad61973b8b1fb16d7574
trbinsc <trbinsc@gmail.com>
2015-07-04 23:16:26 -0400

    First version of blog, plus a bit of style changes

--- config.rb
+++ config.rb
@@ -6,30 +6,34 @@
 
 activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
-  # blog.prefix = "blog"
+   blog.prefix = "blog/"
 
-  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+   blog.permalink = "{year}/{month}/{title}.html"
   # Matcher for blog source files
-  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  blog.sources = "sources/{year}-{month}-{day}-{title}.html"
   # blog.taglink = "tags/{tag}.html"
-  # blog.layout = "layout"
-  # blog.summary_separator = /(READMORE)/
-  # blog.summary_length = 250
-  # blog.year_link = "{year}.html"
-  # blog.month_link = "{year}/{month}.html"
-  # blog.day_link = "{year}/{month}/{day}.html"
+  # blog.layout = "blog_layout"
+   blog.summary_separator = /(READMORE)/
+   blog.year_link = "{year}.html"
+   blog.month_link = "{year}/{month}.html"
+   blog.day_link = "{year}/{month}/{day}.html"
   # blog.default_extension = ".markdown"
 
-  blog.tag_template = "tag.html"
-  blog.calendar_template = "calendar.html"
+  blog.tag_template = "blog/tag.html"
+  blog.calendar_template = "blog/calendar.html"
 
   # Enable pagination
-  # blog.paginate = true
-  # blog.per_page = 10
-  # blog.page_link = "page/{num}"
+   blog.paginate = true
+   blog.per_page = 2
+   blog.page_link = "p{num}"
 end
 
-page "/feed.xml", layout: false
+set :layout, "layout"
+set :blog_layout, "blog_layout"
+
+page "blog/feed.xml", layout: false
+
+page "/blog/*", :layout => :blog_layout
 
 ###
 # Compass
@@ -69,7 +73,7 @@ page "/feed.xml", layout: false
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# activate :livereload
+activate :livereload
 
 # Methods defined in the helpers block are available in templates
 # helpers do
